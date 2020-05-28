2f1d1d64442346567bef5983dd32b854d92d9819
aayus <aayush_shah@branson.org>
2020-05-28 13:15:09 -0700

    first commit

--- resources/views/partials/menus/main-right.blade.php
+++ resources/views/partials/menus/main-right.blade.php
@@ -1,38 +1,48 @@
-<ul>
-    @guest
-    <li><a href="{{ route('register') }}">Sign Up</a></li>
-    <li><a href="{{ route('login') }}">Login</a></li>
-    @else
-    <li>
-        <a href="{{ route('users.edit') }}">My Account</a>
-    </li>
-    <li>
-        <a href="{{ route('logout') }}"
-            onclick="event.preventDefault();
-                     document.getElementById('logout-form').submit();">
-            Logout
-        </a>
-    </li>
+<div class="navbar-tool ml-4">
+    <ul class="navbar-nav mega-nav pr-lg-2 mr-lg-2">
+        @guest
+            <li class="nav-item pr-lg-2 mr-lg-2"><a href="{{ route('register') }}">Sign Up</a></li>
+            <li class="nav-item pr-lg-2 mr-lg-2"><a href="{{ route('login') }}">Login</a></li>
+        @else
+            <li class="nav-item pr-lg-2 mr-lg-2">
+                <a href="{{ route('users.edit') }}">My Account</a>
+            </li>
+            <li>
+                <a href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+                    Logout
+                </a>
+            </li>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                {{ csrf_field() }}
+            </form>
+        @endguest
 
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-        {{ csrf_field() }}
-    </form>
-    @endguest
-    <li><a href="{{ route('cart.index') }}">Cart
-    @if (Cart::instance('default')->count() > 0)
-    <span class="cart-count"><span>{{ Cart::instance('default')->count() }}</span></span>
-    @endif
-    </a></li>
-    {{-- @foreach($items as $menu_item)
-        <li>
-            <a href="{{ $menu_item->link() }}">
-                {{ $menu_item->title }}
-                @if ($menu_item->title === 'Cart')
-                    @if (Cart::instance('default')->count() > 0)
-                    <span class="cart-count"><span>{{ Cart::instance('default')->count() }}</span></span>
-                    @endif
-                @endif
-            </a>
-        </li>
-    @endforeach --}}
-</ul>
+            {{-- <li><a href="{{ route('cart.index') }}">Cart
+             @if (Cart::instance('default')->count() > 0)
+             <span class="cart-count"><span>{{ Cart::instance('default')->count() }}</span></span>
+             @endif
+             </a></li>
+              @foreach($items as $menu_item)
+                 <li>
+                     <a href="{{ $menu_item->link() }}">
+                         {{ $menu_item->title }}
+                         @if ($menu_item->title === 'Cart')
+                             @if (Cart::instance('default')->count() > 0)
+                             <span class="cart-count"><span>{{ Cart::instance('default')->count() }}</span></span>
+                             @endif
+                         @endif
+                     </a>
+                 </li>
+         @endforeach --}}
+    </ul>
+    <a class="navbar-tool-icon-box bg-secondary dropdown-toggle" href="marketplace-cart.html">
+        @if (Cart::instance('default')->count() > 0)
+            <span class="navbar-tool-label">
+                        <span class="cart-count"><span>{{ Cart::instance('default')->count() }}</span></span>
+                    </span>
+        @endif
+        <i class="navbar-tool-icon czi-cart"></i>
+    </a>
+</div>
--- resources/views/partials/menus/main.blade.php
+++ resources/views/partials/menus/main.blade.php
@@ -1,11 +1,10 @@
-<ul>
+<ul class="dropdown-menu py-1">
     @foreach($items as $menu_item)
         <li>
-            <a href="{{ $menu_item->link() }}">
-                {{ $menu_item->title }}
+            <a class="dropdown-item" href="{{ $menu_item->link() }}">{{ $menu_item->title }}</a>
                 @if ($menu_item->title === 'Cart')
                     @if (Cart::instance('default')->count() > 0)
-                    <span class="cart-count"><span>{{ Cart::instance('default')->count() }}</span></span>
+                        <span class="cart-count"><span>{{ Cart::instance('default')->count() }}</span></span>
                     @endif
                 @endif
             </a>
